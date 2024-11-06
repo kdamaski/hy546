884bd39eed4c9fd6fc29303b067b3a7b8fc6cad0
kostasfeed <kostas.damaskinakis@gmail.com>
2024-11-06 14:08:22 +0200

    yaaaR

--- hw3/homework3.ml
+++ hw3/homework3.ml
@@ -7,16 +7,15 @@ open Ast
  *)
 exception Type_error
 (* val lookup: Ast.env -> Ast.var -> Ast.typ *)
-let lookup env var = 
-	let mylookup = function
-  | (v , _) ->
-      if var=v then
-        true
-      else
-        false    
-	in try match List.find mylookup env with (v ,t)-> t  (* if v == var return its type t *)
-		with Not_found -> print_string "no such binding in the environment\n";
-				print_string var; raise (Type_error)
+let lookup (envir : env) (v : var) = 
+  let mysearchpattern env = match env with
+  | (v2,_) ->
+    if v = v2 then
+      true
+    else
+      false
+  in try match List.find mysearchpattern envir with (v2 ,t)-> t  (* if v2 == v return its type t *)
+		with Not_found -> raise (Type_error)
   
 
 (*
@@ -25,3 +24,39 @@ let lookup env var =
  * there can be no type.
  *)
 (* val typeof: Ast.env -> Ast.exp -> Ast.typ *)
+let rec typeof (envir : env) (e : exp) = 
+  match e with
+  | EVar x -> lookup envir x
+  | EVal value ->
+    (match value with
+    | VTrue | VFalse -> TBool
+    | VUnit -> TUnit
+    | VFun (x, t1, e2) ->
+        let env2 = (x, t1)::envir in
+          let t2 = typeof env2 e2 in
+            TFun (t1, t2)
+    )
+  | EApp (e1, e2) -> (* apply e1 (func) with arg e2 *)
+    (let e1_type = typeof envir e1 in
+      match e1_type with
+      | TFun (t, t') ->
+          if (typeof envir e2) = t then t'
+          else raise (Type_error)
+      | _ -> raise (Type_error)
+    )
+  | EIf (e_cond, e_then, e_else) ->
+    (let t_if = (typeof envir e_cond) in
+      if t_if != TBool
+      then raise (Type_error)
+      else if (typeof envir e_then) != (typeof envir e_else)
+        then raise (Type_error)
+      else
+        t_if
+    )
+  | ESeq (e1, e2) ->
+      let t1 = typeof envir e1 in
+        let t2 = typeof envir e2 in
+          if t1 != TUnit then
+            raise (Type_error)
+          else
+            t2
